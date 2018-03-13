3151abfd0f99fa965d6d6c8430b07bf7dcde2e82
ramyaparimi <rlp.parimi@gmail.com>
2018-03-12 21:57:02 -0500

    Courera chapt 1

---
+++ complement.py
@@ -0,0 +1,18 @@
+def Complement(Pattern):
+    x=""
+    for i in Pattern:
+        if i=="A":
+            x=x+"T"
+        elif i=="T":
+            x=x+"A"
+        elif i=="G":
+            x=x+"C"
+        else:
+            x=x+"G"
+    return x
+
+
+
+
+
+print(Complement("ATGCTGAT"))
